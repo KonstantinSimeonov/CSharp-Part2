d22f100a307a91d02741314652567cfe622507ef
Teodor Pulkov <pulkov@gmail.com>
2016-05-15 00:16:07 +0300

    3 and 7 solved

--- homework/02.Multidimensional Arrays/03.Sequence-in-matrix/Program.cs
+++ homework/02.Multidimensional Arrays/03.Sequence-in-matrix/Program.cs
@@ -8,22 +8,128 @@ namespace _03.Sequence_in_matrix
 {
     class Program
     {
-        static void Main(string[] args)
+        static int LongestSequence(string[,] array)
         {
-            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int currentCount = 1;
+            int bestCount = 0;
+            string itemMostCounted = string.Empty;
 
-            int n = input[0];
-            int m = input[1];
-            int[,] array = new int[n, m];
-
-            for (int i = 0; i < n; i++)
+            // check by rows
+            for (int row = 0; row < array.GetLength(0); row++)
             {
-                string[] line = Console.ReadLine().Split(' ');
-                for (int j = 0; j < m; j++)
+                for (int col = 0; col < array.GetLength(1) - 1; col++)
                 {
-                    array[i, j] = int.Parse(line[j]);
+                    if (array[row, col] == array[row, col + 1])
+                    {
+                        currentCount++;
+                    }
+                    else
+                    {
+                        currentCount = 1;
+                    }
+
+                    if (currentCount > bestCount)
+                    {
+                        bestCount = currentCount;
+                        itemMostCounted = array[row, col];
+                    }
+                }
+
+                currentCount = 1;
+            }
+
+            // check by columns
+            for (int col = 0; col < array.GetLength(1); col++)
+            {
+                for (int row = 0; row < array.GetLength(0) - 1; row++)
+                {
+                    if (array[row, col] == array[row + 1, col])
+                    {
+                        currentCount++;
+                    }
+                    else
+                    {
+                        currentCount = 1;
+                    }
+
+                    if (currentCount > bestCount)
+                    {
+                        bestCount = currentCount;
+                        itemMostCounted = array[row, col];
+                    }
+                }
+
+                currentCount = 1;
+            }
+
+            //check by diagonal left to right
+            for (int col = 0; col < array.GetLength(1) - 1; col++)
+            {
+                for (int row = 0, column = col; row < array.GetLength(0) - 1 && column < array.GetLength(1) - 1; row++, column++)
+                {
+                    if ((array[row, column] == array[row + 1, column + 1]))
+                    {
+                        currentCount++;
+                    }
+                    else
+                    {
+                        currentCount = 1;
+                    }
+
+                    if (currentCount > bestCount)
+                    {
+                        bestCount = currentCount;
+                        itemMostCounted = array[row, column];
+                    }
+                }
+
+                currentCount = 1;
+            }
+
+
+            //check by diagonal right to left
+            for (int col = array.GetLength(1) - 1; col > 0; col--)
+            {
+                for (int row = 0, column = col; row < array.GetLength(0) - 1 && column > 0; row++, column--)
+                {
+                    if (array[row, column] == array[row + 1, column - 1])
+                    {
+                        currentCount++;
+                    }
+                    else
+                    {
+                        currentCount = 1;
+                    }
+
+                    if (currentCount > bestCount)
+                    {
+                        bestCount = currentCount;
+                        itemMostCounted = array[row, column];
+                    }
                 }
             }
+
+            // return string.Join(", ", Enumerable.Repeat(itemMostCounted, bestCount).ToArray());
+            return bestCount;
+        }
+
+        static void Main()
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int n = input[0];
+            int m = input[1];
+
+            string[,] array = new string[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                string[] currentLine = Console.ReadLine().Split(' ');
+                for (int j = 0; j < m; j++)
+                {
+                    array[i, j] = currentLine[j];
+                }
+            }
+
+            Console.WriteLine(LongestSequence(array));
         }
     }
 }
---
+++ homework/02.Multidimensional Arrays/07.Largest-area-in-matrix/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Largest_area_in_matrix
+{
+    class LargestObjects
+    {
+        private static bool[,] visited;
+        private static short[,] matrix;
+        private static short maxCount = 0;
+        private static short currentCount = 0;
+        private static short rows;                              
+        private static short cols;                              
+
+        static void Main()
+        {
+            string[] sizes = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            rows = short.Parse(sizes[0]);
+            cols = short.Parse(sizes[1]);
+            
+            matrix = new short[rows, cols];
+            
+            for (short row = 0; row < rows; row++)    
+            {
+                string[] inputRows = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (short col = 0; col < cols; col++) 
+                {
+                    matrix[row, col] = short.Parse(inputRows[col]);
+                }
+            }
+            
+            visited = new bool[rows, cols];
+
+            for (short row = 0; row < rows; row++)
+            {
+                for (short col = 0; col < cols; col++)
+                {
+                    Area(row, col, matrix[row, col]);
+
+                    if (maxCount < currentCount)
+                    {
+                        maxCount = currentCount;
+                    }
+
+                    currentCount = 0;
+                }
+            }
+
+            // Output
+            Console.WriteLine(maxCount);
+        }
+
+        private static void Area(short row, short col, short currentElement)
+        {
+            if ((row < 0) || (row >= rows)
+                || (col < 0) || (col >= cols)
+                || currentElement != matrix[row, col])
+            {
+                return;
+            }
+            
+            if (visited[row, col])
+            {
+                return;
+            }
+            
+            visited[row, col] = true;
+
+            currentCount++;
+
+            Area((short)(row - 1), col, currentElement);
+            Area((short)(row + 1), col, currentElement);
+            Area(row, (short)(col - 1), currentElement);
+            Area(row, (short)(col + 1), currentElement);     
+        }
+
+    }
+}
